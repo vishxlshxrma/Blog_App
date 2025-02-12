704bb9090d41bf9c5d47889f101dddb82685ea6f
Vishal <vishalsharma3657@gmail.comgit>
2025-02-12 08:17:02 -0800

    SQLAlchemy Database Setup

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    firstName = db.Column(db.String(120), nullable = False)
+    lastName = db.Column(db.String(120), nullable = False)
+    password = db.Column(db.String(120), nullable = False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route("/")
 def home():
